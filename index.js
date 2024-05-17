b66bfe8d4d7d46f9880c0033a3a92984a4d185e4
NedovodeevRoman <163845570+NedovodeevRoman@users.noreply.github.com>
2024-05-17 12:13:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,158 @@
+document.getElementById("a").addEventListener("click",function(){
+    document.getElementById("b").classList.add("open")
+})
+document.getElementById("c").addEventListener("click",function(){
+    document.getElementById("b").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b").classList.remove("open")
+    }
+})
+document.querySelector("#b .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+document.getElementById("a1").addEventListener("click",function(){
+    document.getElementById("b1").classList.add("open")
+})
+document.getElementById("c1").addEventListener("click",function(){
+    document.getElementById("b1").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b1").classList.remove("open")
+    }
+})
+document.querySelector("#b1 .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b1").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+document.getElementById("a2").addEventListener("click",function(){
+    document.getElementById("b2").classList.add("open")
+})
+document.getElementById("c2").addEventListener("click",function(){
+    document.getElementById("b2").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b2").classList.remove("open")
+    }
+})
+document.querySelector("#b2 .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b2").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+document.getElementById("a3").addEventListener("click",function(){
+    document.getElementById("b3").classList.add("open")
+})
+document.getElementById("c3").addEventListener("click",function(){
+    document.getElementById("b3").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b3").classList.remove("open")
+    }
+})
+document.querySelector("#b3 .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b3").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+document.getElementById("a4").addEventListener("click",function(){
+    document.getElementById("b4").classList.add("open")
+})
+document.getElementById("c4").addEventListener("click",function(){
+    document.getElementById("b4").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b4").classList.remove("open")
+    }
+})
+document.querySelector("#b4 .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b4").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+document.getElementById("a5").addEventListener("click",function(){
+    document.getElementById("b5").classList.add("open")
+})
+document.getElementById("c5").addEventListener("click",function(){
+    document.getElementById("b5").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b5").classList.remove("open")
+    }
+})
+document.querySelector("#b5 .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b5").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+document.getElementById("a6").addEventListener("click",function(){
+    document.getElementById("b6").classList.add("open")
+})
+document.getElementById("c6").addEventListener("click",function(){
+    document.getElementById("b6").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b6").classList.remove("open")
+    }
+})
+document.querySelector("#b6 .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b6").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+document.getElementById("a7").addEventListener("click",function(){
+    document.getElementById("b7").classList.add("open")
+})
+document.getElementById("c7").addEventListener("click",function(){
+    document.getElementById("b7").classList.remove("open")
+})
+window.addEventListener('keydown', (e)=>{
+    if (e.key === "Escape") {
+        document.getElementById("b7").classList.remove("open")
+    }
+})
+document.querySelector("#b7 .a3e2").addEventListener('click', event =>{
+    event._isClickWithInModal = true;
+});
+document.getElementById("b7").addEventListener('click', event =>{
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
+
+
+
+
+
+
+
